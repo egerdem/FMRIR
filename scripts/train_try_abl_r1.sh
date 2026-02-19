83151f22017a1484d3636660ce629f176a76f182
ege <ege.erdem@gmail.com>
2026-02-19 17:01:26 +0000

    .sh for tokyo reconstruction

--- scripts/train_try_abl_r1.sh
+++ scripts/train_try_abl_r1.sh
@@ -3,10 +3,9 @@
 set -e  # stop on error
 
 python trainer-atf-3d.py \
-    --model_name "KCL_r1_M5_50_freq20_layer3_d512_eta0_head8_sigma1e3_lrWARM5k_e4_toe5_decay300_unet4v1_setv3_300k" \
+    --model_name "KCL_TokyoReconstruct_r1_M5_50_freq20_layer3_d512_eta0_head8_sigma1e3_lrWARM5k_e4_toe5_decay300_unet4v1_setv3_300k" \
     --data_dir /home/ubuntu/EGE/DATA/ir_fs2000_s8192_m1331_room4.0x6.0x3.0_rt200 \
     --experiments_dir /home/ubuntu/EGE/FMRIR_experiments \
-    --idx_mes_pos_path "idx_mes_pos_s1024_m1331.npy"\
     --batch_size 4 \
     --num_iterations 300000 \
     --lr 1e-4 \
@@ -21,7 +20,6 @@ python trainer-atf-3d.py \
     --nhead 8 \
     --num_encoder_layers 3 \
     --M_range 5,50 \
-    --M_val_fixed 5 \
     --eta 0. \
     --sigma 0. \
     --FM_vs_Diff "flow_matching" \
