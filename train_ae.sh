c80efef18b62076729475bbd7ca08e08f140d24b
ege <ege.erdem@gmail.com>
2026-04-02 21:16:35 +0100

    con

--- train_ae.sh
+++ train_ae.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH --job-name=ae_train
 #SBATCH --partition=interruptible_gpu
-#SBATCH --ntasks=2
+#SBATCH --ntasks=1
 #SBATCH --mem=16G
-#SBATCH --time=0-10:00
+#SBATCH --time=0-15:00
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=l40s|h100|a100_80g|a100
 #SBATCH --output=/scratch/users/%u/%j.out
