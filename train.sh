6f6c56a5514707c8df8687d19a14f8d30384eac2
ege <ege.erdem@gmail.com>
2026-04-14 00:22:59 +0100

    bug

--- train.sh
+++ train.sh
@@ -20,8 +20,8 @@ python trainer-atf-3d.py \
     --decay_iterations 100000 \
     --num_iterations 400000 \
     --model_name "KCL_TimeMeanDefault_fctx_UnetV2res_freq20_Val102_Mval5_r1_M5_10_20_50_layer3_d512_eta0_head8_sigma0_lrWARM5k_e4_toe5_decay100_setv3_unet3_256to1024v2_400k" \
-    --data_dir scratch/users/k24037994/EGE/DATA/ir_fs2000_s8192_m1331_room4.0x6.0x3.0_rt200 \
-    --experiments_dir scratch/users/k24037994/EGE/FMRIR_experiments \
+    --data_dir /scratch/users/k24037994/EGE/DATA/ir_fs2000_s8192_m1331_room4.0x6.0x3.0_rt200 \
+    --experiments_dir /scratch/users/k24037994/EGE/FMRIR_experiments \
     --batch_size 4 \
     --lr 1e-4 \
     --min_lr 1e-5 \
--- trainer-atf-3d.py
+++ trainer-atf-3d.py
@@ -372,7 +372,7 @@ if __name__ == '__main__':
     parser.add_argument('--save_start_iter', type=int, default=0,
                         help='Skip saving model checkpoints until this iteration. Avoids frequent saves early in training.')
     parser.add_argument('--freq_up_to', type=int, default=64, help='Upper freq bin (exclusive upper bound of subband)')
-    parser.add_argument('--freq_from', type=int, default=30, help='Lower freq bin (inclusive). Default 0 = full range from DC. Set e.g. 20 to train on bins 20..freq_up_to only.')
+    parser.add_argument('--freq_from', type=int, default=0, help='Lower freq bin (inclusive). Default 0 = full range from DC. Set e.g. 20 to train on bins 20..freq_up_to only.')
     parser.add_argument('--eta', type=float, help='Probability for CFG dropout.', default=0.1)
     parser.add_argument('--sigma', type=float, help='Sigma for noise in the path.', default=0)
     parser.add_argument('--loss_type', type=str, default='standard',
