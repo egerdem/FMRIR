935fa3497d96fa80a511c1facbf99641d78baa78
ege <ege.erdem@gmail.com>
2026-04-14 17:41:48 +0100

    krr config data path fix

--- AUTOENCODER/src/configs.py
+++ AUTOENCODER/src/configs.py
@@ -3560,7 +3560,7 @@ config_KRR_10007 = {
         "freq_from": 0,
         "freq_up_to": 20,
         "init_delay": False,
-        "dataset": ['ir_fs2000_s1024_m1331_room4.0x6.0x3.0_rt200'],
+        "dataset": ['ir_fs2000_s8192_m1331_room4.0x6.0x3.0_rt200'],
         "src_index": {
             'ir_fs2000_s1024_m1331_room4.0x6.0x3.0_rt200': {
                 'train': range(0, 820),
